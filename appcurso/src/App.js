72a4b22edd6e1c874177149bda2bc18d0e576a14
Paulo Andre <pauloandre050619@gmail.com>
2025-04-26 17:08:43 -0300

    feat: implementando page com funcionalidade de dar like

--- appcurso/src/App.js
+++ appcurso/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [likes, setlikes] = useState(0);
+
+  const DaroLike = () => {
+    setlikes(likes + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='row'>
+        <div className='col'>
+          <p>Quantidade de likes:{likes}</p>
+        </div>
+        <div className='col'>
+          <input type='button' value="likes" onClick={DaroLike}></input>
+        </div>
+        <div className='col'>
+
+        </div>
+      </div>
     </div>
   );
 }
